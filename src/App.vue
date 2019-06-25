6aa89d7511f802395e5e09a6b91200b9d12388d2
Bjorkbat <bjorkbat@gmail.com>
2019-06-25 14:05:42 -0600

    Added text area component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
   <div id="app">
-    <form>
-      <FormTextInput :placeholder="'Name'"/>
+    <form style="max-width: 400px;">
+      <FormTextInput placeholder="Name"/>
+      <FormTextArea placeholder="Message" rows="6"/>
     </form>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-
 
   </div>
 </template>
 
 <script>
 import FormTextInput from './components/atoms/FormTextInput.vue';
+import FormTextArea from './components/atoms/FormTextArea.vue';
 import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'app',
   components: {
+    FormTextArea,
     FormTextInput,
     HelloWorld
   }
---
+++ src/components/atoms/FormTextArea.vue
@@ -0,0 +1,17 @@
+<!-- a highly stylized textarea element -->
+<template>
+	<div class="vt-form-text">
+		<textarea
+			:placeholder="placeholder"
+			:rows="rows"
+			:value="value"
+			@input="$emit('input', $event.target.value)"/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "FormTextArea",
+	props: ['placeholder', 'rows', 'value']
+}
+</script>
