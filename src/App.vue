e8b4b53cae42dc7c37c2d4bd69fcc3c3dd55661e
Bjorkbat <bjorkbat@gmail.com>
2019-06-25 14:34:38 -0600

    Working on button styling.  Holding off on filll and transition effects for now

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <form style="max-width: 400px;">
       <FormTextInput placeholder="Name"/>
       <FormTextArea placeholder="Message" rows="6"/>
+      <GradientButton>
+        Send
+      </GradientButton>
     </form>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
@@ -13,12 +16,14 @@
 <script>
 import FormTextInput from './components/atoms/FormTextInput.vue';
 import FormTextArea from './components/atoms/FormTextArea.vue';
+import GradientButton from './components/atoms/GradientButton.vue';
 import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'app',
   components: {
     FormTextArea,
     FormTextInput,
+    GradientButton,
     HelloWorld
   }
 }
