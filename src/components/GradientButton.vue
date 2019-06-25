5915a2458be6ed685b969fb1330480dadd562279
Bjorkbat <bjorkbat@gmail.com>
2019-06-25 10:40:35 -0600

    Added dependencies for sass, gonna look at form inputs

---
+++ src/components/GradientButton.vue
@@ -0,0 +1,18 @@
+<!-- Your standard, default button.  This will likely be renamed in the future -->
+<template>
+	<button class="vt-btn">
+		<slot></slot>
+	</button>
+</template>
+
+<script>
+export default {
+	name "GradientButton"
+}
+</script>
+
+<style lang="scss" scoped>
+.vt-btn {
+	cursor: pointer;
+}
+</style>
---
+++ src/components/atoms/FormTextInput.vue
@@ -0,0 +1,9 @@
+<!-- Component for rendering a form input -->
+<template>
+</template>
+
+<script>
+export default {
+	name: "FormTextInput"
+}
+</script>
