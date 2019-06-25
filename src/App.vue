ac68637c6837e73cdb5478a642c3cd699bd7203e
Bjorkbat <bjorkbat@gmail.com>
2019-06-25 11:16:11 -0600

    First pass at form input, not looking at gradients yet

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <form>
+      <FormTextInput :placeholder="'Name'"/>
+    </form>
   </div>
 </template>
 
 <script>
+import FormTextInput from './components/atoms/FormTextInput.vue';
 import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   components: {
+    FormTextInput,
     HelloWorld
   }
 }
--- src/components/atoms/FormTextInput.vue
+++ src/components/atoms/FormTextInput.vue
@@ -1,9 +1,20 @@
 <!-- Component for rendering a form input -->
 <template>
+
+	<div class="vt-form-text">
+
+		<input type="text"
+			:placeholder="placeholder"
+			:value="value"
+			@input="$emit('input', $event.target.value)"/>
+
+	</div>
+
 </template>
 
 <script>
 export default {
-	name: "FormTextInput"
+	name: "FormTextInput",
+	props: ['placeholder', 'value']
 }
 </script>
