82c867e8ed945572e6457a373a84a501730e8ba4
Bjorkbat <bjorkbat@gmail.com>
2019-06-27 16:16:38 -0600

    Added component for floating labels

---
+++ src/components/atoms/FloatingFormLabel.vue
@@ -0,0 +1,15 @@
+<!-- A form label designed to "float" over a form input. -->
+<template>
+	<label class="vt-floating-label" :for="elementId">{{ name }}</label>
+</template>
+
+<script>
+export default {
+	name: "FloatingFormLabel",
+	props: ['element-id', 'name']
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
