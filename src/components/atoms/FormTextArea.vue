951847b7adfaf305ba36cb98782d4994d4ff6280
Bjorkbat <bjorkbat@gmail.com>
2019-06-27 16:17:24 -0600

    Modified formtextarea for using an optional floating label

--- src/components/atoms/FormTextArea.vue
+++ src/components/atoms/FormTextArea.vue
@@ -1,17 +1,57 @@
 <!-- a highly stylized textarea element -->
 <template>
 	<div class="vt-form-text">
+
 		<textarea
-			:placeholder="placeholder"
+			:class="{'has-content': value != ''}"
+			:placeholder="showPlaceholder ? placeholder: ''"
+			:required="required"
 			:rows="rows"
 			:value="value"
+			@focus="togglePlaceholder(true)"
+			@blur="togglePlaceholder(false)"
 			@input="$emit('input', $event.target.value)"/>
+
+		<slot></slot>
+
 	</div>
 </template>
 
 <script>
 export default {
 	name: "FormTextArea",
-	props: ['placeholder', 'rows', 'value']
+	props: ['placeholder', 'required', 'rows', 'value'],
+
+	data: function() {
+		return {
+			focused: false,
+			showPlaceholder: false,
+		}
+	},
+
+	methods: {
+
+		togglePlaceholder: function(isFocused) {
+
+			this.focused = isFocused;
+
+			if (isFocused && this.value == '') {
+				this.showPlaceholder = true;
+				return;
+			}
+
+			this.showPlaceholder = false;
+
+		}
+
+	},
+
+	watch: {
+
+		value: function() {
+			this.togglePlaceholder(this.focused);
+		}
+
+	}
 }
 </script>
