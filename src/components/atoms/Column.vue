673ab1e15e73b3f0ea7ac5784041040781598e47
Bjorkbat <bjorkbat@gmail.com>
2019-06-28 11:10:48 -0600

    Gonna go ahead and add these basic rows and columns

---
+++ src/components/atoms/Column.vue
@@ -0,0 +1,21 @@
+<!-- At it's base, a div with a class of "col", but does have props for sizing
+	purposes.  Props are optional, you can just feed it more classes -->
+<template>
+	<div class="col">
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Column",
+	props: ['size', 'offset']
+}
+</script>
+
+<style lang="scss" scoped>
+.col {
+	flex-basis: 0;
+	flex-grow: 1;
+	max-width: 100%;
+}
+</style>
---
+++ src/components/atoms/Row.vue
@@ -0,0 +1,19 @@
+<!-- Literally just a div with a class="row" -->
+<template>
+	<div class="row">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Row"
+}
+</script>
+
+<style lang="scss" scoped>
+.row {
+	display: flex;
+	flex-wrap: wrap;
+}
+</style>
