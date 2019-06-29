b6dd1f130ad438fc3c00d9f21b9eb34593956cc6
Bjorkbat <bjorkbat@gmail.com>
2019-06-28 19:36:26 -0600

    Container now uses breakpoint mixins

--- src/components/atoms/Container.vue
+++ src/components/atoms/Container.vue
@@ -10,3 +10,31 @@ export default {
 	name: "Container"
 }
 </script>
+
+<style lang="scss">
+@import '../../styles/breakpoints';
+.container {
+	width: 100%;
+	padding-right: 1.5rem;
+	padding-left: 1.5rem;
+	margin-right: auto;
+	margin-left: auto;
+
+	@include smartphone {
+		max-width: 540px;
+	}
+
+	@include tablet {
+		max-width: 720px;
+	}
+
+	@include desktop {
+		max-width: 960px;
+	}
+
+	@include xl-desktop {
+		max-width: 1140px;
+	}
+	
+}
+</style>
