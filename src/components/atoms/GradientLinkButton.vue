4ff33d7991b80f2eaa6c3bf9cf0ee14a5ec71fd8
Bjorkbat <bjorkbat@gmail.com>
2019-07-01 12:45:50 -0600

    Came up with some rather interesting hacks for this button

---
+++ src/components/atoms/GradientLinkButton.vue
@@ -0,0 +1,15 @@
+<!-- Variation of the the GradientButton.  Looks like a button, quacks like a
+	link.  You know, because it is one. -->
+<template>
+	<router-link class="vt-gradient-btn" :class="{filled}"
+		:to="{path}">
+		{{ name }}
+	</router-link>
+</template>
+
+<script>
+export default {
+	name: "GradientLinkButton",
+	props: ['filled', 'name', 'path']
+}
+</script>
--- src/components/molecules/Navbar.vue
+++ src/components/molecules/Navbar.vue
@@ -7,41 +7,64 @@
 		</div>
 
 		<!-- Links proper.  Fits on the right-hand side -->
-		<div class="menu">
-			<router-link v-for="page in pages" class="h6"
-				:to="{ path: page.path}">
-				{{ page.name }}
-			</router-link>
+		<div class="navbar-right">
+
+			<div class="menu">
+				<router-link v-for="page in pages" class="h6"
+					:to="{ path: page.path}">
+					{{ page.name }}
+				</router-link>
+			</div>
+
+			<GradientLinkButton
+				class="offset-bottom"
+				:filled="true"
+				path=""
+				name="Get a quote"/>
 		</div>
 
 	</div>
 </template>
 
 <script>
+import GradientLinkButton from '../atoms/GradientLinkButton.vue';
 export default {
 	name: "Navbar",
-	props: ['pages']
+	props: ['pages'],
+	components: { GradientLinkButton }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../../styles/colors';
 .navbar {
-	background-color: $purple-dark;
+	background-color: lighten($purple-dark, 7%);
 	height: 7.5rem;
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+
+	.navbar-right {
+		display: flex;
+		align-items: flex-end;
+	}
 
 	.menu {
-		align-items: center;
-		display: flex;
-		float: right;
+		display: inline-flex;
 		height: 100%;
+		align-items: center;
+
+		a {
+			color: $white;
+			display: block;
+			flex-grow: 0;
+			padding: 2rem;
+		}
 	}
 
-	a {
-		color: $white;
-		display: block;
-		flex-grow: 0;
-		padding: 2rem;
+	.vt-gradient-btn {
+		display: inline-block;
 	}
+
 }
 </style>
