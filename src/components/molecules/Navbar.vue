c5ace2e2bc9eebe31d7b01351d3c49aae04cdcd8
Bjorkbat <bjorkbat@gmail.com>
2019-08-23 11:17:50 -0600

    Conditionally rendering mobile nav drawer

--- src/components/molecules/Navbar.vue
+++ src/components/molecules/Navbar.vue
@@ -1,19 +1,22 @@
 <!-- A navbar designed to fit at the top -->
 <template>
-	<div class="navbar">
+
+	<div class="navbar" :class="{'mobile-nav': $mq == 'smartphone' || $mq == 'tablet'}">
 
 		<div class="brand">
 			<img src="../../assets/vt-logo-wide.png"/>
 		</div>
 
 		<!-- Links proper.  Fits on the right-hand side -->
-		<div class="navbar-right">
+		<div class="navbar-right" v-if="$mq == 'desktop' || $mq =='xl'">
 
 			<div class="menu">
+
 				<router-link v-for="page in pages" class="h6"
 					:to="{ path: page.path}">
 					{{ page.name }}
 				</router-link>
+
 			</div>
 
 			<GradientLinkButton
@@ -21,9 +24,38 @@
 				:filled="true"
 				path=""
 				name="Get a quote"/>
+
 		</div>
 
+		<!-- Mobile drawer -->
+		<div class="mobile-drawer" v-if="drawer">
+
+			<div class="brand">
+				<img src="../../assets/vt-logo-wide.png"/>
+			</div>
+
+			<GradientLinkButton
+				class="offset-bottom"
+				:filled="true"
+				path=""
+				name="Get a quote"/>
+
+			<router-link v-for="page in pages" class="h6"
+				:to="{ path: page.path}">
+				{{ page.name }}
+			</router-link>
+
+		</div>
+
+		<a class="hamburger" v-if="$mq == 'smartphone' || $mq == 'tablet'"
+			@click.prevent="drawer = !drawer">
+			<i class="material-icons">
+				menu
+			</i>
+		</a>
+
 	</div>
+
 </template>
 
 <script>
@@ -31,7 +63,14 @@ import GradientLinkButton from '../atoms/GradientLinkButton.vue';
 export default {
 	name: "Navbar",
 	props: ['pages'],
-	components: { GradientLinkButton }
+	components: { GradientLinkButton },
+
+	data: function() {
+		return {
+			drawer: false
+		}
+	}
+
 }
 </script>
 
@@ -71,6 +110,12 @@ export default {
 			flex-grow: 0;
 			padding: 2rem;
 		}
+	}
+
+	.hamburger {
+		margin-right: 3rem;
+		color: $white;
+		cursor: pointer;
 	}
 
 	.vt-gradient-btn {
