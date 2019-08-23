aa2a63573a2457adc370e2934adb564801d4b98d
Bjorkbat <bjorkbat@gmail.com>
2019-08-23 11:17:21 -0600

    Added vue-mq

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
+import VueMQ from 'vue-mq';
 import App from './App.vue'
 import router from './router';
 
 Vue.config.productionTip = false
+Vue.use(VueMQ, {
+  breakpoints: {
+    smartphone: 576,
+    tablet: 768,
+    desktop: 992,
+    xl: Infinity
+  }
+});
 
 new Vue({
   render: h => h(App),
   router
-}).$mount('#app')
+}).$mount('#app');
